5e61227f40ca347a695df9629500c4a6455237e8
avanich28 <arpawan.v@gmail.com>
2024-03-22 23:29:20 +0700

    solve webhook error

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -64,11 +64,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // Topic: Finishing Payments with Stripe Webhooks
-app.post(
-  '/webhook-checkout',
-  express.raw({ type: 'application/json' }),
-  bookingController.webhookCheckout,
-);
+app.post('/webhook-checkout', express.raw(), bookingController.webhookCheckout);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
